3da4526ae8b94336b6fd43c4e664be2428e9f82d
flaymond100 <flaymond100@gmail.com>
2021-04-08 10:54:22 +0300

    second commit

--- main.js
+++ main.js
@@ -29,10 +29,10 @@ class Bycicle{
         let slicedName = name.slice(0,15);
         return `<div class="accordion-item">
         <h2 class="accordion-header" id="heading${slicedName.replace(/\s+/g, '')}">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${slicedName.replace(/\s+/g, '')}" aria-expanded="false" aria-controls="collapse${slicedName.replace(/\s+/g, '')}">
-            <strong>${this.name}</strong>
+         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${slicedName.replace(/\s+/g, '')}" aria-expanded="false" aria-controls="collapse${slicedName.replace(/\s+/g, '')}">
+         <img class="accordion_picture" src="./images/${this.name.replace(/\s+/g, '')}.jpg" > <strong>${this.name}</strong>
           </button>
-          <img src="./images/${this.name.replace(/\s+/g, '')}".jpg width="20" height="20">
+          
         </h2>
         <div id="collapse${slicedName.replace(/\s+/g, '')}" class="accordion-collapse collapse collapse" aria-labelledby="heading${slicedName.replace(/\s+/g, '')}" data-bs-parent="#accordionBycycle">
           <div class="accordion-body">
